323cf2afef40254bea0afaff4eb45e66a29a99aa
Adel Chouanine <a.chouanine@hotmail.fr>
2018-04-02 00:06:26 +0200

    add user post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates  :name, presence: true
+	validates  :email, presence: true
+	has_many :posts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+
   root 'users#index'
   resources :users
 
